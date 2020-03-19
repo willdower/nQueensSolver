6025db2e081d0d6de49fdbe13d5b5ad544b85e97
William Dower <william.dower@griffithuni.edu.au>
2020-03-19 22:11:55 +1000

    Added unpruned and pruned functions with timing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <cmath>
+#include <chrono>
 
 class boardState {
 public:
@@ -74,13 +75,93 @@ std::vector<boardState*> unprunedBFSReturnBoards(short n) {
         }
 
         for (int i=0;i<n;i++) {
+            newBoard = new boardState(currentBoard, n);
+            newBoard->board[newBoard->numQueens] = i;
+            newBoard->numQueens++;
+            queue.push(newBoard);
+        }
+        delete currentBoard;
+        currentBoard = nullptr;
+    }
+
+    return goalStates;
+
+}
+
+int unprunedBFSReturnSolutions(short n) {
+    int solutions = 0;
+
+    boardState *currentBoard;
+    boardState *newBoard;
+
+    std::queue<boardState*> queue;
+
+    queue.push(new boardState(n));
+
+    while (!queue.empty()) {
+        currentBoard = queue.front();
+        queue.pop();
+        if (currentBoard->numQueens == n) {
+            if (checkGoalState(currentBoard, n)) {
+                solutions++;
+                delete currentBoard;
+                currentBoard = nullptr;
+                continue;
+            }
+            else {
+                delete currentBoard;
+                currentBoard = nullptr;
+                continue;
+            }
+        }
+
+        for (int i=0;i<n;i++) {//Potential options of next queen
+            newBoard = new boardState(currentBoard, n);
+            newBoard->board[newBoard->numQueens] = i;
+            newBoard->numQueens++;
+            queue.push(newBoard);
+        }
+        delete currentBoard;
+        currentBoard = nullptr;
+    }
+
+    return solutions;
+
+}
+
+std::vector<boardState*> prunedBFSReturnBoards(short n) {
+
+    std::vector<boardState*> goalStates;
+    boardState *currentBoard;
+    boardState *newBoard;
+
+    std::queue<boardState*> queue;
+
+    queue.push(new boardState(n));
+
+    while (!queue.empty()) {
+        currentBoard = queue.front();
+        queue.pop();
+        if (currentBoard->numQueens == n) {
+            if (checkGoalState(currentBoard, n)) {
+                goalStates.push_back(currentBoard);
+                continue;
+            }
+            else {
+                delete currentBoard;
+                currentBoard = nullptr;
+                continue;
+            }
+        }
+
+        for (int i=0;i<n;i++) {//Potential options of next queen
             int validBoard = true;
             for (int j=0;j<currentBoard->numQueens;j++) { //Scanning through current boards queen positions
                 if (i == currentBoard->board[j]) {
                     validBoard = false;
                     break;
                 }
-                if (abs(j-i) == abs(currentBoard->board[j]-currentBoard->board[i])) {
+                if (currentBoard->board[j] == i) {
                     validBoard = false;
                     break;
                 }
@@ -103,7 +184,7 @@ std::vector<boardState*> unprunedBFSReturnBoards(short n) {
 
 }
 
-int unprunedBFSReturnSolutions(short n, long long & states) {
+int prunedBFSReturnSolutions(short n) {
     int solutions = 0;
 
     boardState *currentBoard;
@@ -116,10 +197,11 @@ int unprunedBFSReturnSolutions(short n, long long & states) {
     while (!queue.empty()) {
         currentBoard = queue.front();
         queue.pop();
-        states++;
         if (currentBoard->numQueens == n) {
             if (checkGoalState(currentBoard, n)) {
                 solutions++;
+                delete currentBoard;
+                currentBoard = nullptr;
                 continue;
             }
             else {
@@ -161,31 +243,55 @@ int unprunedBFSReturnSolutions(short n, long long & states) {
 
 int main() {
 
+    std::cout << "PRUNED BFS to 20" << std::endl << std::endl;
+
     for (int i=4;i<21;i++) {
-        if (i < -1) {
-            std::vector<boardState*> goalStates = unprunedBFSReturnBoards(i);
-            std::cout << i << std::endl;
+        if (i < 7) {
+            auto startBFS = std::chrono::system_clock::now();
+            std::vector<boardState*> goalStates = prunedBFSReturnBoards(i);
+            auto endBFS = std::chrono::system_clock::now();
+            std::chrono::duration<double> timeTaken = endBFS - startBFS;
+            std::cout << i << " - Time Taken: " << timeTaken.count() << std::endl;
             for (auto state : goalStates) {
                 for (int j=0;j<i;j++) {
-                    std::cout << "(" << j << ", " << state->board[j] << ") ";
+                    std::cout << static_cast<char>(j+97) << state->board[j] << " ";
                 }
                 std::cout << std::endl;
             }
-            std::cout << std::endl;
         }
         else {
-            long long states = 0;
-            std::cout << i << " - " << unprunedBFSReturnSolutions(i, states) << " States: " << states << std::endl;
+            auto startBFS = std::chrono::system_clock::now();
+            int solutions = prunedBFSReturnSolutions(i);
+            auto endBFS = std::chrono::system_clock::now();
+            std::chrono::duration<double> timeTaken = endBFS - startBFS;
+            std::cout << i << " - " << solutions << " - Time Taken: " << timeTaken.count();
         }
+        std::cout << std::endl;
     }
 
-    /**boardState *test = new boardState(5);
-    test->board[0] = 1;
-    test->board[1] = 3;
-    test->board[2] = 0;
-    test->board[3] = 2;
-    test->board[4] = 4;
-    test->numQueens = 5;
+    std::cout << "UNPRUNED BFS to 20" << std::endl << std::endl;
 
-    std::cout << sizeof(test);**/
+    for (int i=4;i<21;i++) {
+        if (i < 7) {
+            auto startBFS = std::chrono::system_clock::now();
+            std::vector<boardState*> goalStates = unprunedBFSReturnBoards(i);
+            auto endBFS = std::chrono::system_clock::now();
+            std::chrono::duration<double> timeTaken = endBFS - startBFS;
+            std::cout << i << " - Time Taken: " << timeTaken.count() << std::endl;
+            for (auto state : goalStates) {
+                for (int j=0;j<i;j++) {
+                    std::cout << static_cast<char>(j+97) << state->board[j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+        else {
+            auto startBFS = std::chrono::system_clock::now();
+            int solutions = unprunedBFSReturnSolutions(i);
+            auto endBFS = std::chrono::system_clock::now();
+            std::chrono::duration<double> timeTaken = endBFS - startBFS;
+            std::cout << i << " - " << solutions << " - Time Taken: " << timeTaken.count();
+        }
+        std::cout << std::endl;
+    }
 }
